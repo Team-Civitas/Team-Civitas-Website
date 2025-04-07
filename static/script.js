388779db50ce5554f5554dc709c1c3f2903ebb5a
DanteMinecraft <dantewallen07@gmail.com>
2025-04-07 23:26:50 +0200

    Bytt ut bilder till webp, fixat automatiserad javascript för bilder

--- static/script.js
+++ static/script.js
@@ -1,21 +1,39 @@
 // För Synergy-bilderna
 // Kör en funktion när hela html-filen är färdigladdad
+
 document.addEventListener("DOMContentLoaded", function () {
-    const grid = document.getElementById("portfolio-bilder");
-    const imageCount = 31; //Antal bilder - ÄNDRA NÄR FLER LÄGGS TILL
+  const grid = document.getElementById("portfolio-bilder");
+  let index = 1;
 
-    for (let i = 1; i <= imageCount; i++) {
-      const img = document.createElement("img");
-      img.src = `../static/img/portfolio/synergy/${i}.png`;
-      img.alt = `Bild - ${i}`;
-      img.style.cursor = "pointer";
+  function createImageElement(src, alt) {
+    const img = document.createElement("img");
 
-      // Öppnar bilden i ny flik när man klickar på bilden
-      img.addEventListener("click", () => {
-        window.open(img.src, "_blank");
-      });
+    img.src = src;
+    img.alt = alt;
+    img.style.cursor = "pointer";
 
-      // Lägger in den skapade bilden under portfolio-grid elementet
-      grid.appendChild(img);
+    // Öppnar bilden i ny flik när man klickar på bilden
+    img.addEventListener("click", () => {
+      window.open(img.src, "_blank");
+    });
+
+    // Lägger in den skapade bilden under portfolio-grid elementet
+    grid.appendChild(img);
+  }
+
+  async function countImages() {
+    while (true) {
+      const url = `../static/img/portfolio/synergy/${index}.webp`;
+      try {
+        const response = await fetch(url);
+        if (!response.ok) break;
+        createImageElement(url, `Bild - ${index}`);
+        index++;
+      } catch (error) {
+        break;
+      }
     }
-  });
+  }
+
+  countImages();
+});
