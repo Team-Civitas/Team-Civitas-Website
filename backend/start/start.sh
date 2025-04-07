280dfaf67c3fe536aca9990ea374ad83be41a156
Revilo0509 <olivers_gamecenter@outlook.com>
2025-04-07 16:43:32 +0200

    Fix gunicorn chdir path in start.sh script

--- backend/start/start.sh
+++ backend/start/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/TeamCivitas/Team-Civitas-Repo
-gunicorn --chdir backend backend:app --bind 0.0.0.0:1515 --workers 3
+gunicorn --chdir /home/TeamCivitas/Team-Civitas-Repo backend:app --bind 0.0.0.0:1515 --workers 3
