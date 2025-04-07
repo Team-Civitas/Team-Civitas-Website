81fdafd72a16606583c297ab504ad1136e52553a
Revilo0509 <olivers_gamecenter@outlook.com>
2025-04-07 21:01:00 +0200

    Update update.sh to run service restart in the background

--- backend/update.sh
+++ backend/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/TeamCivitas/Team-Civitas-Repo
 git pull origin main
-systemctl restart teamcivitas.service
-echo "Updated teamcivitas website successfully."
+systemctl restart teamcivitas.service &
