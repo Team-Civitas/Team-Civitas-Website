40b2912e8235b21df7199d2e0bfe63288cfae13d
Revilo0509 <156201838+Revilo0509@users.noreply.github.com>
2025-04-07 16:22:46 +0200

    Update start.sh

--- backend/start/start.sh
+++ backend/start/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Navigate to the backend directory
-cd "$(dirname "$0")/../backend"
-
-# Start the Gunicorn web server in production mode
-gunicorn backend:backend --bind 0.0.0.0:8000 --workers 3 --env ENV=production --port 1515
+gunicorn --chdir backend backend:app --bind 0.0.0.0:1515 --workers 3
