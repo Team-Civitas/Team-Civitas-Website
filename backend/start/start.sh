01a3288a293746cd2e2c3d6f7129d4633d3d9c06
DanteMinecraft <dantewallen07@gmail.com>
2025-04-11 01:11:21 +0200

    Test fix (Re-add)

--- backend/start/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/TeamCivitas/Team-Civitas-Repo
-gunicorn --chdir /home/TeamCivitas/Team-Civitas-Repo backend.backend:app --bind 0.0.0.0:1515 --workers 3
--- backend/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/TeamCivitas/Team-Civitas-Repo
-git pull origin main
-systemctl restart teamcivitas.service &
