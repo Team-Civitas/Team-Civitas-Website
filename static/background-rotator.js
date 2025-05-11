d38fd0ef40befb4ab9153017cd5ab10db43d595e
Revilo0509 <olivers_gamecenter@outlook.com>
2025-05-11 22:36:00 +0200

    idk

--- static/background-rotator.js
+++ static/background-rotator.js
@@ -1,13 +1,36 @@
+async function countImages(url, prefix) {
+    let index = 1;
+    let count = 0;
 
-const el = document.getElementById("bg");
-let index = 0;
+    while (true) {
+        const current_url = `${url}/${prefix} (${index}).webp`;
+        try {
+            const response = await fetch(current_url);
+            if (!response.ok) throw new Error("Image not found");
 
-function changeBackground(index= 0) {
-    el.style.background = `url('/static/img/portfolio/synergy/synergy (17).webp')`;
-    index = (index + 1) % images.length;
-  }
+            count++;
+            index++;
+        } catch (error) {
+            break;
+        }
+    }
 
-window.addEventListener('DOMContentLoaded', () => {
-    changeBackground(); // Initial set
-    setInterval(changeBackground, 10000);
-  });
+    console.log(`Found ${count} images.`);
+    return count;
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const el = document.getElementById("background-image");
+    const imageCount = await countImages("../static/img/featured_imgs", "featured_imgs");
+
+    function changeBackground() {
+        const index = getRandomIntInclusive(1, imageCount);
+        el.style.backgroundImage = `url('/static/img/featured_imgs/featured_imgs (${index}).webp')`;
+    }
+
+    changeBackground();
+});
