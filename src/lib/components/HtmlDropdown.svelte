1560e9a9247789518045238af270420e97ab6f24
Revilo0509 <olivers_gamecenter@outlook.com>
2025-08-15 18:14:31 +0200

    added delay

--- src/lib/components/HtmlDropdown.svelte
+++ src/lib/components/HtmlDropdown.svelte
@@ -3,18 +3,36 @@
 
 	let { children, name } = $props();
 	let active = $state(false);
+	let timeout;
 
 	function toggleMenu() {
+		clearTimeout(timeout);
 		active = !active;
+	}
+
+	function handleMouseLeave() {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			active = false;
+		}, 300);
+	}
+
+	function handleMouseEnter() {
+		clearTimeout(timeout);
 	}
 </script>
 
 <div>
-	<div class="html-dropdown" role="menu" tabindex="0" onmouseleave={() => (active = false)}>
+	<div
+		class="html-dropdown"
+		role="menu"
+		tabindex="0"
+		onmouseleave={handleMouseLeave}
+		onmouseenter={handleMouseEnter}
+	>
 		<button type="button" aria-label="mega-menu" onclick={toggleMenu}>
 			{name}<i class="fa fa-caret-down" style="margin-left: 7.5px;"></i>
 		</button>
-
 		<div class="dropdown-content {active ? 'active' : ''}">{@render children()}</div>
 	</div>
 </div>
