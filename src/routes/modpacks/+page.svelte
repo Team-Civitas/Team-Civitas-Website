b547e53c7b8db9512bfb433bc5877e2e27c27721
DanteMinecraft <dantewallen07@gmail.com>
2025-08-15 22:03:08 +0200

    Co-authored-by: Revilo0509 <Revilo0509@users.noreply.github.com>

--- src/routes/modpacks/+page.svelte
+++ src/routes/modpacks/+page.svelte
@@ -3,13 +3,22 @@
 	import HorizontalScroll from '$lib/components/HorizontalScroll.svelte';
 	import TitledCard from '$lib/components/TitledCard.svelte';
 	import { logotype } from '$lib/IconManager';
+
+	const iconImports = import.meta.glob("$lib/assets/Images/ModpackIcons/**/*", { eager: true, as: "url" });
+	let icons = Object.values(iconImports);
 </script>
 
-<Header title="MODPACKS" />
+<Header title="MODPACKS"/>
 
 <div class="container-wide">
 	<h1 class="heading heading-left">THE CIVITAS STORIES</h1>
 	<HorizontalScroll>
+		{#each icons as icon}
+			<TitledCard 
+				src={icon}
+				title="TOOO"
+			/>
+		{/each}
 		<TitledCard
 			src="https://starlightskins.lunareclipse.studio/render/ultimate/DanteMinecraft/full"
 			title="Civitas: Overdrive"
