1f3ba137797aeeb77e41b3c3a1502ea28bd6671b
DanteMinecraft <dantewallen07@gmail.com>
2025-08-15 19:52:58 +0200

    fixed about us

--- src/routes/om-oss/+page.svelte
+++ src/routes/om-oss/+page.svelte
@@ -8,13 +8,41 @@
 
 <div>
 	<h2 class="heading heading-center">TEAMET BAKOM CIVITAS</h2>
-	<hr />
-	<h3 class="subheading subheading-center">GRUNDARE</h3>
-
-	<TitledCard
-		src="https://starlightskins.lunareclipse.studio/render/ultimate/DanteMinecraft/full"
-	/>
+	<div>
+		<h3 class="subheading subheading-center">GRUNDARE</h3>
+		<TitledCard
+			src="https://starlightskins.lunareclipse.studio/render/ultimate/DanteMinecraft/full"
+		/>
+	</div>
+	<div>
+		<h4 class="subheading subheading-center">CIVITAS CORE</h4>
+		<div class="civitas-core">
+			<TitledCard
+				src="https://starlightskins.lunareclipse.studio/render/ultimate/Elf_Oliver/full"
+			/>
+			<TitledCard
+				src="https://starlightskins.lunareclipse.studio/render/ultimate/Lulle2345/full"
+			/>
+			<TitledCard
+				src="https://starlightskins.lunareclipse.studio/render/ultimate/Zcloak/full"
+			/>
+		</div>
+	</div>
 </div>
 
 <style>
+
+	h2 {
+		padding: 70px;
+	}
+
+	.subheading {
+		padding: 20px;
+	}
+
+	.civitas-core {
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
 </style>
