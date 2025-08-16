6928edd19cf107efabe1b2107d237ed4f63eb498
Revilo0509 <olivers_gamecenter@outlook.com>
2025-08-16 21:57:18 +0200

    added markdown

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelte({
+		extensions: ['.svelte', '.md'],
+		preprocess: markdown()
+	})],
 	server: {
 		host: true,
 		allowedHosts: ['dev.revilo0509.net']
