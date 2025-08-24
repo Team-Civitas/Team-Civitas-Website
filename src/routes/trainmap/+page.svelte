66a469d4042a79e4279f1f2dc3bbec434a27de2b
DanteMinecraft <dantewallen07@gmail.com>
2025-08-24 23:22:44 +0200

    portat trainmap

---
+++ src/routes/trainmap/+page.svelte
@@ -0,0 +1,13 @@
+<div class="trainmap-wrapper">
+	<iframe src="https://trainmap.teamcivitas.net" frameborder="0"></iframe>
+</div>
+
+<style>
+	iframe {
+		height: 100vh;
+		width: 100vw;
+		overflow: hidden !important;
+
+		border: none;
+	}
+</style>
