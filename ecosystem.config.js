ba0ab37622f24b3a674fbe510daec100bd72a6fb
Revilo0509 <olivers_gamecenter@outlook.com>
2025-08-29 22:20:14 +0200

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "teamcivitas",
+      script: "build/index.js",
+      env: {
+        PORT: 4000,
+        HOST: "0.0.0.0"
+      }
+    }
+  ]
+};
