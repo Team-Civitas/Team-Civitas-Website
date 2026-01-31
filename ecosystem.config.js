b564948bc0eba8b75e5f5a659e669eecad28bf59
Revilo0509 <olivers_gamecenter@outlook.com>
2026-02-01 00:11:51 +0100

    Wat

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,14 @@
-export const apps = [
+module.exports = {
+  apps: [
     {
-        name: "teamcivitas",
-        script: "build/index.js",
-        env: {
-            PORT: 4000,
-            HOST: "0.0.0.0"
-        }
+      name: "teamcivitas",
+      script: "build/index.js",
+      env: {
+        PORT: 4000,
+        HOST: "0.0.0.0",
+        NODE_ENV: "production",
+        ORIGIN: "https://teamcivitas.net" // Recommended for SvelteKit
+      }
     }
-];
+  ]
+}
